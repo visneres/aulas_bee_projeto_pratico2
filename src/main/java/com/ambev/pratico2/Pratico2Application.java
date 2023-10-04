529cb892d9e00ded071843a8f5064c7709e356af
Emerson Fonseca <idd_emefonseca@uolinc.com>
2023-10-04 17:57:08 -0300

    Inicial

---
+++ src/main/java/com/ambev/pratico2/Pratico2Application.java
@@ -0,0 +1,13 @@
+package com.ambev.pratico2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratico2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratico2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambev/pratico2/Pratico2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ambev.pratico2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratico2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Funcionou..");
+	}
+
+}
